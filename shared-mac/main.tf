22d00926c37b7c8bcfcfd101d7587bc86b4c7aa6
Ben <ben@coder.com>
2022-06-12 23:45:06 +0000

    add basic template for shared mac

---
+++ shared-mac/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = "0.4.2"
+    }
+  }
+}
+variable "mac_user" {
+  sensitive = true
+}
+
+variable "mac_host" {
+  sensitive = true
+}
+
+
+provider "aws" {
+  region = var.region
+}
+
+data "coder_workspace" "me" {
+}
+
+resource "coder_agent" "dev" {
+  arch = "amd64"
+  auth = "token"
+  os   = "darwin"
+}
+
+resource "null_resource" "mac-ssh" {
+  count = data.coder_workspace.me.start_count == 0 ? 0 : 1
+  connection {
+    type = "ssh"
+    user = var.mac_user
+    host = var.mac_host
+    # agent = true can also be used, but this was easier
+    private_key = file("/root/.ssh/id_rsa")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # 1) Start Coder
+      "CODER_AGENT_URL=${data.coder_workspace.me.access_url} CODER_AGENT_AUTH=token CODER_AGENT_TOKEN=${coder_agent.dev.token} nohup /usr/local/bin/coder agent &",
+      # 2) Gracefully exit "terraform apply"
+      # see https://stackoverflow.com/a/36732953
+      "sleep 1"
+    ]
+  }
+}
+
+resource "null_resource" "stop-coder-agent" {
+  count = data.coder_workspace.me.start_count == 0 ? 1 : 0
+  connection {
+    type = "ssh"
+    user = var.mac_user
+    host = var.mac_host
+    # agent = true can also be used, but this was easier
+    private_key = file("/root/.ssh/id_rsa")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # stop Coder agent
+      "kill $(ps -eo pid,pgid,tpgid,args | awk 'NR == 1 || ($3 != -1 && $2 != $3)' | grep \"coder agent\" | awk '{ print $1}')"
+    ]
+  }
+}
+
