c2044f8bef65cf64c373d488e5de19b5301717e2
Ben <ben@coder.com>
2022-06-16 20:20:07 +0000

    add kubernetes-custom-image

---
+++ kubernetes-custom-image/main.tf
@@ -0,0 +1,131 @@
+terraform {
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = "~> 0.4.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.10"
+    }
+  }
+}
+
+variable "use_kubeconfig" {
+  type        = bool
+  sensitive   = true
+  description = <<-EOF
+  Use host kubeconfig? (true/false)
+
+  Set this to false if the Coder host is itself running as a Pod on the same
+  Kubernetes cluster as you are deploying workspaces to.
+
+  Set this to true if the Coder host is running outside the Kubernetes cluster
+  for workspaces.  A valid "~/.kube/config" must be present on the Coder host. This
+  is likely not your local machine unless you are using `coder server --dev.`
+
+  EOF
+}
+
+variable "workspaces_namespace" {
+  type        = string
+  sensitive   = true
+  description = "The namespace to create workspaces in (must exist prior to creating workspaces)"
+  default     = "coder-workspaces"
+}
+
+provider "kubernetes" {
+  # Authenticate via ~/.kube/config or a Coder-specific ServiceAccount, depending on admin preferences
+  config_path = var.use_kubeconfig == true ? "~/.kube/config" : null
+}
+
+data "coder_workspace" "me" {}
+
+# code-server
+resource "coder_app" "code-server" {
+  agent_id      = coder_agent.dev.id
+  name          = "code-server"
+  icon          = "/code.svg"
+  url           = "http://localhost:13337"
+  relative_path = true
+}
+
+
+resource "coder_agent" "dev" {
+  os   = "linux"
+  arch = "amd64"
+  dir  = "/home/coder"
+  startup_script =<<EOF
+    #!/bin/sh
+    curl -fsSL https://code-server.dev/install.sh | sh
+    code-server --auth none --port 13337
+  EOF
+}
+
+variable "disk_size" {
+  description = "Disk size (__ GB)"
+  default     = 10
+}
+
+variable "docker_image" {
+  description = <<EOF
+  Name of Docker image to pull
+  
+  e.g codercom/enterprise-base:ubuntu
+  EOF
+  default     = "codercom/enterprise-base:ubuntu"
+}
+
+resource "kubernetes_pod" "main" {
+  count = data.coder_workspace.me.start_count
+  depends_on = [
+    kubernetes_persistent_volume_claim.home-directory
+  ]
+  metadata {
+    name      = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
+    namespace = var.workspaces_namespace
+  }
+  spec {
+    security_context {
+      run_as_user = 1000
+      fs_group    = 1000
+    }
+    container {
+      name    = "dev"
+      image   = "${var.docker_image}"
+      command = ["sh", "-c", coder_agent.dev.init_script]
+      security_context {
+        run_as_user = "1000"
+      }
+      env {
+        name  = "CODER_AGENT_TOKEN"
+        value = coder_agent.dev.token
+      }
+      volume_mount {
+        mount_path = "/home/coder"
+        name       = "home-directory"
+      }
+    }
+    volume {
+      name = "home-directory"
+      persistent_volume_claim {
+        claim_name = kubernetes_persistent_volume_claim.home-directory.metadata.0.name
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "home-directory" {
+  metadata {
+    name      = "coder-pvc-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
+    namespace = var.workspaces_namespace
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "${var.disk_size}Gi"
+      }
+    }
+  }
+}
