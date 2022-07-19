85a8f1d10a65f035e08902d8e126b3055929ff7e
Ben <ben@coder.com>
2022-07-19 12:58:12 +0000

    fixes to custom image template

--- kubernetes-custom-image/main.tf
+++ kubernetes-custom-image/main.tf
@@ -45,20 +45,48 @@ data "coder_workspace" "me" {}
 resource "coder_app" "code-server" {
   agent_id      = coder_agent.dev.id
   name          = "code-server"
-  icon          = "/code.svg"
+  icon          = "/icon/code.svg"
   url           = "http://localhost:13337"
   relative_path = true
 }
 
 
 resource "coder_agent" "dev" {
-  os   = "linux"
-  arch = "amd64"
-  dir  = "/home/coder"
-  startup_script =<<EOF
+  os             = "linux"
+  arch           = "amd64"
+  dir            = "/home/coder"
+  startup_script = <<EOF
     #!/bin/sh
     curl -fsSL https://code-server.dev/install.sh | sh
-    code-server --auth none --port 13337
+    code-server --auth none --port 13337 s&
+
+#!/bin/bash
+# install projector
+PROJECTOR_BINARY=/home/coder/.local/bin/projector
+if [ -f $PROJECTOR_BINARY ]; then
+    echo 'projector has already been installed - check for update'
+    /home/coder/.local/bin/projector self-update 2>&1 | tee projector.log
+else
+    echo 'installing projector'
+    pip3 install projector-installer --user 2>&1 | tee projector.log
+fi
+echo 'access projector license terms'
+/home/coder/.local/bin/projector --accept-license 2>&1 | tee -a projector.log
+PROJECTOR_CONFIG_PATH=/home/coder/.projector/configs/projector1
+if [ -d "$PROJECTOR_CONFIG_PATH" ]; then
+    echo 'projector has already been configured and the JetBrains IDE downloaded - skip step' 2>&1 | tee -a projector.log
+else
+    echo 'autoinstalling IDE and creating projector config folder'
+
+    /home/coder/.local/bin/projector ide autoinstall --config-name "projector1" --ide-name "IntelliJ IDEA Community Edition 2021.3" --hostname=localhost --port 8997 --use-separate-config --password coder 2>&1 | tee -a projector.log
+    # delete the configuration's run.sh input parameters that check password tokens since tokens do not work with coder_app yet passed in the querystring
+    grep -iv "HANDSHAKE_TOKEN" $PROJECTOR_CONFIG_PATH/run.sh > temp && mv temp $PROJECTOR_CONFIG_PATH/run.sh 2>&1 | tee -a projector.log
+    chmod +x $PROJECTOR_CONFIG_PATH/run.sh 2>&1 | tee -a projector.log
+    echo "creation of intellij configuration complete" 2>&1 | tee -a projector.log
+fi
+
+# start JetBrains projector-based IDE
+/home/coder/.local/bin/projector run projector1 &
   EOF
 }
 
@@ -92,7 +120,7 @@ resource "kubernetes_pod" "main" {
     }
     container {
       name    = "dev"
-      image   = "${var.docker_image}"
+      image   = var.docker_image
       command = ["sh", "-c", coder_agent.dev.init_script]
       security_context {
         run_as_user = "1000"
@@ -128,4 +156,15 @@ resource "kubernetes_persistent_volume_claim" "home-directory" {
       }
     }
   }
+}
+
+resource "coder_app" "projector" {
+  agent_id      = coder_agent.dev.id
+  name          = "Projector"
+  icon          = "/icon/projector.svg"
+  url           = "http://localhost:8997/"
+  relative_path = true
+
+  # Only show when using an image name containing "multieditor"
+  count = contains(regex("^(?:.*(multieditor))?.*$", var.docker_image), "multieditor") == true ? 1 : 0
 }
