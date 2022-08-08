6130f54881382ccd7b77cedc1250519a34cc142f
Ben <ben@coder.com>
2022-08-08 15:57:12 +0000

    fixups to kubernetes template

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = "~> 0.3.1"
+      version = "~> 0.4.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -48,7 +48,38 @@ resource "coder_agent" "go" {
   startup_script = <<EOF
     #!/bin/sh
     curl -fsSL https://code-server.dev/install.sh | sh
-    code-server --auth none --port 13337
+    code-server --auth none --port 13337 & 
+
+    # install projector
+    PROJECTOR_BINARY=/home/vscode/.local/bin/projector
+    if [ -f $PROJECTOR_BINARY ]; then
+        echo 'projector has already been installed - check for update'
+        /home/vscode/.local/bin/projector self-update 2>&1 | tee projector.log
+    else
+        echo 'installing projector'
+        pip3 install projector-installer --user 2>&1 | tee projector.log
+    fi
+
+    echo 'access projector license terms'
+    /home/vscode/.local/bin/projector --accept-license 2>&1 | tee -a projector.log
+
+    PROJECTOR_CONFIG_PATH=/home/vscode/.projector/configs/goland
+
+    if [ -d "$PROJECTOR_CONFIG_PATH" ]; then
+        echo 'projector has already been configured and the JetBrains IDE downloaded - skip step' 2>&1 | tee -a projector.log
+    else
+        echo 'autoinstalling IDE and creating projector config folder'
+        /home/vscode/.local/bin/projector ide autoinstall --config-name "goland" --ide-name "GoLand 2021.3" --hostname=localhost --port 8997 --use-separate-config --password coder 2>&1 | tee -a projector.log
+
+        # delete the configuration's run.sh input parameters that check password tokens since tokens do not work with coder_app yet passed in the querystring
+        grep -iv "HANDSHAKE_TOKEN" $PROJECTOR_CONFIG_PATH/run.sh > temp && mv temp $PROJECTOR_CONFIG_PATH/run.sh 2>&1 | tee -a projector.log
+        chmod +x $PROJECTOR_CONFIG_PATH/run.sh 2>&1 | tee -a projector.log
+
+        echo "creation of goland configuration complete" 2>&1 | tee -a projector.log
+    fi
+
+    # start JetBrains projector-based IDE
+    /home/vscode/.local/bin/projector run goland &
   EOF
 }
 
@@ -60,7 +91,39 @@ resource "coder_agent" "java" {
     #!/bin/sh
     curl -fsSL https://code-server.dev/install.sh | sh
     code-server --auth none --port 13337
-  EOF
+
+    # install projector
+    PROJECTOR_BINARY=/home/vscode/.local/bin/projector
+    if [ -f $PROJECTOR_BINARY ]; then
+        echo 'projector has already been installed - check for update'
+        /home/vscode/.local/bin/projector self-update 2>&1 | tee projector.log
+    else
+        echo 'installing projector'
+        pip3 install projector-installer --user 2>&1 | tee projector.log
+    fi
+
+    echo 'access projector license terms'
+    /home/vscode/.local/bin/projector --accept-license 2>&1 | tee -a projector.log
+
+    PROJECTOR_CONFIG_PATH=/home/vscode/.projector/configs/intellij
+
+    if [ -d "$PROJECTOR_CONFIG_PATH" ]; then
+        echo 'projector has already been configured and the JetBrains IDE downloaded - skip step' 2>&1 | tee -a projector.log
+    else
+        echo 'autoinstalling IDE and creating projector config folder'
+        /home/vscode/.local/bin/projector ide autoinstall --config-name "intellij" --ide-name "IntelliJ IDEA Community Edition 2021.3" --hostname=localhost --port 8997 --use-separate-config --password coder 2>&1 | tee -a projector.log
+
+        # delete the configuration's run.sh input parameters that check password tokens since tokens do not work with coder_app yet passed in the querystring
+        grep -iv "HANDSHAKE_TOKEN" $PROJECTOR_CONFIG_PATH/run.sh > temp && mv temp $PROJECTOR_CONFIG_PATH/run.sh 2>&1 | tee -a projector.log
+        chmod +x $PROJECTOR_CONFIG_PATH/run.sh 2>&1 | tee -a projector.log
+
+        echo "creation of intellij configuration complete" 2>&1 | tee -a projector.log
+    fi
+
+    # start JetBrains projector-based IDE
+    /home/vscode/.local/bin/projector run intellij &
+
+EOF
 }
 
 resource "coder_agent" "ubuntu" {
@@ -88,22 +151,22 @@ resource "coder_app" "goland" {
   agent_id      = coder_agent.go.id
   name          = "GoLand"
   icon          = "/icon/goland.svg"
-  url           = "http://localhost:13337"
+  url           = "http://localhost:8997"
   relative_path = true
 }
 
-# goland
+# intellij
 resource "coder_app" "intellij" {
   agent_id      = coder_agent.java.id
   name          = "IntelliJ IDEA"
   icon          = "/icon/goland.svg"
-  url           = "http://localhost:13337"
+  url           = "http://localhost:8997"
   relative_path = true
 }
 
 # code-server
 resource "coder_app" "code-server2" {
-  agent_id      = coder_agent.ubuntu.dev.id
+  agent_id      = coder_agent.ubuntu.id
   name          = "code-server"
   icon          = "/icon/code.svg"
   url           = "http://localhost:13337"
