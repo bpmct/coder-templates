7c3ec04d9c1608e7901d7894c52d5a68ae4093e7
Ben <ben@coder.com>
2022-08-10 09:10:18 +0000

    fix k8s stuff

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -11,22 +11,6 @@ terraform {
   }
 }
 
-variable "use_kubeconfig" {
-  type        = bool
-  sensitive   = true
-  description = <<-EOF
-  Use host kubeconfig? (true/false)
-
-  Set this to false if the Coder host is itself running as a Pod on the same
-  Kubernetes cluster as you are deploying workspaces to.
-
-  Set this to true if the Coder host is running outside the Kubernetes cluster
-  for workspaces.  A valid "~/.kube/config" must be present on the Coder host. This
-  is likely not your local machine unless you are using `coder server --dev.`
-
-  EOF
-}
-
 variable "workspaces_namespace" {
   type        = string
   sensitive   = true
@@ -35,8 +19,7 @@ variable "workspaces_namespace" {
 }
 
 provider "kubernetes" {
-  # Authenticate via ~/.kube/config or a Coder-specific ServiceAccount, depending on admin preferences
-  config_path = var.use_kubeconfig == true ? "~/.kube/config" : null
+  config_path = "~/.kube/config"
 }
 
 data "coder_workspace" "me" {}
@@ -49,6 +32,8 @@ resource "coder_agent" "go" {
     #!/bin/sh
     curl -fsSL https://code-server.dev/install.sh | sh
     code-server --auth none --port 13337 & 
+
+    sudo apt-get update && sudo apt-get install -y python3 python3-pip
 
     # install projector
     PROJECTOR_BINARY=/home/vscode/.local/bin/projector
@@ -90,7 +75,9 @@ resource "coder_agent" "java" {
   startup_script = <<EOF
     #!/bin/sh
     curl -fsSL https://code-server.dev/install.sh | sh
-    code-server --auth none --port 13337
+    code-server --auth none --port 13337 &
+
+    sudo apt-get update && sudo apt-get install -y python3 python3-pip
 
     # install projector
     PROJECTOR_BINARY=/home/vscode/.local/bin/projector
@@ -167,7 +154,7 @@ resource "coder_app" "intellij" {
 # code-server
 resource "coder_app" "code-server2" {
   agent_id      = coder_agent.ubuntu.id
-  name          = "code-server"
+  name          = "VS Code"
   icon          = "/icon/code.svg"
   url           = "http://localhost:13337"
   relative_path = true
